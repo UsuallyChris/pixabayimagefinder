6d6eb6fe22d5a55d8871d1b4c31ea7579cae39e1
UsuallyChris <schoonc@gmail.com>
2019-03-19 17:49:55 -0700

    Added navbar.

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+function NavBar() {
+  return(
+    <AppBar title="PixaBay Image Finder" />
+  );
+}
+
+export default NavBar;
